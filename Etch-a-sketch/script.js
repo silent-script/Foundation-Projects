9858176bbc638f2e1cf20330ff1665bc1d6fdfc9
silent-script <saurabh.kumar77@outlook.com>
2024-07-29 23:02:29 +0530

    started project

---
+++ Etch-a-sketch/script.js
@@ -0,0 +1,6 @@
+let container = document.querySelector(".container");
+for (let index = 0; index < 5; index++) {
+    for (let i = 0; i < 5; i++) {
+        container.innerHTML += `<div class="grid">`;
+    }
+}
