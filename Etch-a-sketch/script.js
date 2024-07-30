018ca26a424a593254805ba17d2faa955c0f83f4
silent-script <saurabh.kumar77@outlook.com>
2024-07-30 23:29:24 +0530

    crack grid logic

--- Etch-a-sketch/script.js
+++ Etch-a-sketch/script.js
@@ -1,6 +1,15 @@
-let container = document.querySelector(".container");
+let container = document.querySelector("#container");
+
+
+
 for (let index = 0; index < 5; index++) {
+    let line = document.createElement('div');
+    line.setAttribute('class', 'line');
+    container.appendChild(line);
+
     for (let i = 0; i < 5; i++) {
-        container.innerHTML += `<div class="grid">`;
+        let element = document.createElement('div')
+        element.setAttribute('class', 'collumn');
+        line.appendChild(element);
     }
 }
